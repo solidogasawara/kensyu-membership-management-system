65eea2a362ae6bf8b49b94a5c61adc9333a559da
YoshihisaSaito <y.saito@solidseed.co.jp>
2023-06-13 10:09:36 +0900

    first commit

---
+++ kensyu/About.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="kensyu.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>あなたのアプリケーションの説明ページ。</h3>
+    <p>ここに追加情報を提供してください。</p>
+
+    <button id="SearchButton">検索</button>
+
+    <div id="ResultsArea">
+        <!-- 検索結果はここにJavaScriptによって表示されます -->
+    </div>
+
+    <script src="About.js"></script>
+</asp:Content>
---
+++ kensyu/About.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#SearchButton').click(function (e) {
+        e.preventDefault(); // Prevent the default form submission
+        console.log("SearchButton clicked");
+        $.ajax({
+            type: 'POST',
+            url: 'About.aspx/GetSearchResultsForWebMethod',
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                var results = JSON.parse(data.d); // .d is used in ASP.NET WebForms to access the data.
+
+                // Process the received data here. For example, display it in a GridView.
+                // This will be dependent on your specific implementation
+            }
+        });
+    });
+});
